0a9f77766d494fc01162689c1bd5ec223c20954b
Larry Agbana <larryagbana@gmail.com>
2023-05-29 23:17:41 -0400

    Create data stack instantiating a dynamodb table

---
+++ url-shortner/src/infra/stacks/dataStack.ts
@@ -0,0 +1,25 @@
+import { Stack } from "aws-cdk-lib";
+import { Table as DynamoTable, AttributeType } from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+import { getSuffixFromStack } from "../util";
+
+import { type StackProps } from "aws-cdk-lib";
+import { type ITable } from "aws-cdk-lib/aws-dynamodb";
+
+export class DataStack extends Stack {
+  public readonly shortnerTable: ITable;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const suffix = getSuffixFromStack(this);
+
+    this.shortnerTable = new DynamoTable(this, "ShortnerTable", {
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+      tableName: `ShortnerTable-${suffix}`,
+    });
+  }
+}
