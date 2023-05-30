c2a6e3a9136888adf46e117c02f9dcf52ca99e5f
Larry Agbana <larryagbana@gmail.com>
2023-05-29 23:28:21 -0400

    Integrate stacks in app

---
+++ url-shortner/src/infra/launcher.ts
@@ -0,0 +1,14 @@
+import { App, Aspects } from "aws-cdk-lib";
+import { ApiStack } from "./stacks/apiStack";
+import { LambdaStack } from "./stacks/lambdaStack";
+import { DataStack } from "./stacks/dataStack";
+
+const app = new App();
+
+const dataStack = new DataStack(app, "DataStack");
+const lambdaStack = new LambdaStack(app, "LambdaStack", {
+  shortnerTable: dataStack.shortnerTable,
+});
+new ApiStack(app, "ApiStack", {
+  lambdaIntegration: lambdaStack.lambdaIntegration,
+});
