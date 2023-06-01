0310e2a0860e36dcdcf98c6ae49d1cd2abf0dc6f
Larry Agbana <larryagbana@gmail.com>
2023-06-01 16:03:36 -0400

    Url validation

---
+++ url-shortner/client/src/utils/index.test.ts
@@ -0,0 +1,13 @@
+import { validateUrl } from "./";
+
+describe("Utils", () => {
+  it("validates input as url", () => {
+    const entry = "https://www.google.com/";
+    expect(validateUrl(entry)).toBe("https://www.google.com/");
+  });
+
+  it("invalidates non-url", () => {
+    const entry = "google";
+    expect(validateUrl(entry)).toBe(null);
+  });
+});
