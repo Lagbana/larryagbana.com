8f1824e2457e2d60b53fb78144259c4c39ae59c6
Larry Agbana <larryagbana@gmail.com>
2023-06-27 11:51:42 -0400

    Enable copy shortned url to clipboard

--- aws-cdk/client/src/components/Toast.tsx
+++ aws-cdk/client/src/components/Toast.tsx
@@ -1,4 +1,14 @@
 import { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const ToastContainer = styled.span`
+  display: inline-flex;
+  justify-content: center;
+  margin-top: 4px;
+  padding: 4px 8px;
+  border-radius: 4px;
+  background-color: #98e898;
+`;
 
 export const Toast = ({ message = "", duration = 3000 }) => {
   const [visible, setVisible] = useState(true);
@@ -17,5 +27,9 @@ export const Toast = ({ message = "", duration = 3000 }) => {
     return null;
   }
 
-  return <div className='toast'>{message}</div>;
+  return (
+    <aside role='alert'>
+      <ToastContainer>{message}</ToastContainer>
+    </aside>
+  );
 };
--- aws-cdk/client/src/utils/index.ts
+++ aws-cdk/client/src/utils/index.ts
@@ -1,4 +1,6 @@
+import { useState, useCallback, useEffect } from "react";
 import { z } from "zod";
+import copy from "copy-to-clipboard";
 
 const UrlSchema = z.string().url();
 
@@ -8,4 +10,31 @@ export function validateUrl(url: string): string | null {
   } catch {
     return null;
   }
+}
+
+/**
+ * React hook to copy content to clipboard
+ *
+ * @param text the text or value to copy
+ * @param timeout delay (in ms) to switch back to initial state once copied.
+ */
+export function useClipboard(text: string, timeout = 1500) {
+  const [hasCopied, setHasCopied] = useState(false);
+
+  const onCopy = useCallback(() => {
+    const didCopy = copy(text);
+    setHasCopied(didCopy);
+  }, [text]);
+
+  useEffect(() => {
+    if (hasCopied) {
+      const id = setTimeout(() => {
+        setHasCopied(false);
+      }, timeout);
+
+      return () => clearTimeout(id);
+    }
+  }, [timeout, hasCopied]);
+
+  return { value: text, onCopy, hasCopied };
 }
